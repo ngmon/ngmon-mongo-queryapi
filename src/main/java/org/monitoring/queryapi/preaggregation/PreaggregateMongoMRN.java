8b32d1abfcf56328c8b500625d87d42f6e85cb8a
Michal <Michal@Michal-ntb>
2013-05-07 15:37:14 +0200

    mrn incremental map reduce from temporary collection, after than copy to persistant

--- src/main/java/org/monitoring/queryapi/preaggregation/PreaggregateMongoMRN.java
+++ src/main/java/org/monitoring/queryapi/preaggregation/PreaggregateMongoMRN.java
@@ -5,15 +5,18 @@ import com.google.code.morphia.Morphia;
 import com.mongodb.BasicDBObject;
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MapReduceCommand;
 import com.mongodb.MapReduceOutput;
 import com.mongodb.WriteConcern;
+import java.awt.Cursor;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.monitoring.queryapi.Event;
 import org.monitoring.queryapi.Field;
@@ -42,31 +45,30 @@ public class PreaggregateMongoMRN implements Preaggregate {
         int countForMR = 10;
         Boolean fromBottom = true;
         morphia.map(Event.class);
-        Datastore ds = morphia.createDatastore(col.getDB().getMongo(), col.getDB().toString());
-        ds.save(event);
+        DBObject object = morphia.toDBObject(event);
+        col.save(object);
+        col.getDB().getCollection("temp").save(object);
 
-        DBObject query = BasicDBObjectBuilder.start()
-                .push("to").append("$lte", event.getDate()).pop()
-                .push("count").append("$lt", countForMR)
-                .get();
-        DBObject update = BasicDBObjectBuilder.start()
-                .push("$inc").append("count", 1)
-                .pop().push("$set").append("to", event.getDate())
-                .pop().push("$setOnInsert").append("from", event.getDate())
-                .get();
-        DBObject counter = col.getDB().getCollection("counter")
-                .findAndModify(query, null, new BasicDBObject("_id", -1), false, update, true, true);
-        
-        if (((Integer) counter.get("count")) % countForMR == 0) {
-            col.getDB().eval("var count = 0;"
-                    + "var cursor = db.counter.find();"
-                + "while ( cursor.hasNext() ){"
-                + "var o = cursor.next();"
-                + "count++;"
-                + "db.temp.save( o );"
-                + "}");
+        /*DBObject query = BasicDBObjectBuilder.start()
+         .push("to").append("$lte", event.getDate()).pop()
+         .push("count").append("$lt", countForMR)
+         .get();
+         DBObject update = BasicDBObjectBuilder.start()
+         .push("$inc").append("count", 1)
+         .pop().push("$set").append("to", event.getDate())
+         .pop().push("$setOnInsert").append("from", event.getDate())
+         .get();
+         DBObject counter = col.getDB().getCollection("counter")
+         .findAndModify(query, null, new BasicDBObject("_id", -1), false, update, true, true);*/
+
+        if (col.getDB().getCollection("temp").count() % countForMR == 0) {
+            DBCollection temp = col.getDB().getCollection("temp");
+            DBCollection temp2 = col.getDB().getCollection("temp" + event.getDate().getTime());
+            DBObject o;
+            while ((o = temp.findAndModify(null, null, null, true, null, true, false)) != null) {
+                temp2.save(o);
+            }
             //col.getDB().getCollection("counter").remove(new BasicDBObject("count", 10), WriteConcern.NORMAL);
-            System.out.println("MR" + counter.get("from") + counter.get("to"));
             for (int i = 0; i < times.length; i++) {
                 int before = 0;
                 if (i > 0) {
@@ -88,27 +90,26 @@ public class PreaggregateMongoMRN implements Preaggregate {
 
                 for (int k = -rangeLeft; k <= rangeRight; k++) {
                     BasicDBObjectBuilder queryLocal = new BasicDBObjectBuilder();
-                    queryLocal.push("date").append(Field.GTE, (Date) counter.get("from")).append(Field.LT, (Date) counter.get("to"));
+                    //queryLocal.push("date").append(Field.GTE, (Date) counter.get("from")).append(Field.LT, (Date) counter.get("to"));
 
                     String range = "";
                     if (rangeLeft != 0 || rangeRight != 0) {
                         range = ".l" + rangeLeft + ".r" + rangeRight;
                     }
                     String outputCol = colName + actual + range;
-
+                    DBCollection inputCol = temp2;
                     MapReduceCommand mapReduceCmd =
-                            new MapReduceCommand(col, map, reduce, outputCol,
+                            new MapReduceCommand(inputCol, map, reduce, outputCol,
                             MapReduceCommand.OutputType.REDUCE, queryLocal.get());
 
                     Map<String, Object> globalVariables = new HashMap<String, Object>();
                     globalVariables.put("actualMillis", unit.toMillis(actual));
                     globalVariables.put("nextMillis", unit.toMillis(next));
                     mapReduceCmd.setScope(globalVariables);
-                    mapReduceCmd.setSort(new BasicDBObject("_id", -1));
-                    mapReduceCmd.setLimit(countForMR);
 
-                    MapReduceOutput out = col.mapReduce(mapReduceCmd);
+                    MapReduceOutput out = inputCol.mapReduce(mapReduceCmd);
                     int x = 2 + 1;
+                    inputCol.drop();
                 }
             }
         }
